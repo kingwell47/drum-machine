d44c9a5e036932dc4d6507a4438e61db130b1f00
kingwell47 <joeldoctor47@gmail.com>
2021-09-08 11:39:10 +0800

    edited soundlist

--- src/audio/index.js
+++ src/audio/index.js
@@ -1,23 +1,13 @@
-import sound01 from "../audio/270304__littlerobotsoundfactory__collect-point-00.wav";
-import sound02 from "../audio/270307__littlerobotsoundfactory__explosion-01.wav";
-import sound03 from "../audio/270317__littlerobotsoundfactory__jump-01.wav";
-import sound04 from "../audio/270326__littlerobotsoundfactory__hit-01.wav";
-import sound05 from "../audio/270336__littlerobotsoundfactory__shoot-02.wav";
-import sound06 from "../audio/270337__littlerobotsoundfactory__pickup-00.wav";
-import sound07 from "../audio/270340__littlerobotsoundfactory__pickup-01.wav";
-import sound08 from "../audio/270341__littlerobotsoundfactory__pickup-04.wav";
-import sound09 from "../audio/270342__littlerobotsoundfactory__pickup-03.wav";
-
-let soundList = {
-    1: sound01,
-    2: sound02,
-    3: sound03,
-    4: sound04,
-    5: sound05,
-    6: sound06,
-    7: sound07,
-    8: sound08,
-    9: sound09
+const soundList = {
+    sound01: require("../audio/270304__littlerobotsoundfactory__collect-point-00.wav").default,
+    sound02: require("../audio/270307__littlerobotsoundfactory__explosion-01.wav").default,
+    sound03: require("../audio/270317__littlerobotsoundfactory__jump-01.wav").default,
+    sound04: require("../audio/270326__littlerobotsoundfactory__hit-01.wav").default,
+    sound05: require("../audio/270336__littlerobotsoundfactory__shoot-02.wav").default,
+    sound06: require("../audio/270337__littlerobotsoundfactory__pickup-00.wav").default,
+    sound07: require("../audio/270340__littlerobotsoundfactory__pickup-01.wav").default,
+    sound08: require("../audio/270341__littlerobotsoundfactory__pickup-04.wav").default,
+    sound09: require("../audio/270342__littlerobotsoundfactory__pickup-03.wav").default
 }
 
 export default soundList;
